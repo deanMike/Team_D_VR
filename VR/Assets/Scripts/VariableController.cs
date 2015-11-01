1884eb2f541b5b3c2e163bcc8826094b95e7755c
Mike Dean <msd14@hampshire.edu>
2015-11-01 07:46:16 -0500

    Fixed Errors

--- VR/Assets/Scripts/VariableController.cs
+++ VR/Assets/Scripts/VariableController.cs
@@ -1,46 +1,41 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class VariableController : MonoBehaviour {
-
     public float gameTime;
     public int numProjectiles;
     public bool loadGame = false;
     public float projectileSpeed = 100;
     private GameObject Panel;
-	private Transform DFPos;
+    private Transform DFPos;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    private void Start() {
         DontDestroyOnLoad(gameObject);
         Panel = GameObject.Find("Panel");
         Panel.SetActive(false);
-		if (Application.loadedLevel.Equals(1)) {
-			DFPos = GameObject.Find ("DragonFly").GetComponent<Transform>();
-		}
+        if (Application.loadedLevel.Equals(1)) {
+            DFPos = GameObject.Find("DragonFly").GetComponent<Transform>();
+        }
     }
-	
-	// Update is called once per frame
-	void Update () {
-	    if (loadGame)
-        {
+
+    // Update is called once per frame
+    private void Update() {
+        if (loadGame) {
             loadGame = false;
             Application.LoadLevel(1);
             Panel.SetActive(true);
-		} 
+        }
         if (Application.loadedLevel.Equals(2)) {
             Panel.SetActive(false);
         }
-		if (Application.loadedLevel.Equals(1)) {
-			for (int i = 0; i < 5; i++) {
-				Invoke("SpawnDragonfly", 2.0f);
-		}
-		}
+        //if (Application.loadedLevel.Equals(1)) {
+        //    for (int i = 0; i < 5; i++) {
+        //        Invoke("SpawnDragonfly", 2.0f);
+        //    }
+        //}
+    }
 
-	}
-
-	public void SpawnDragonfly() {
-		Instantiate (Dragonfly, DFPos.position, DFPos.rotation);
-	}
-
+    public void SpawnDragonfly() {
+        //	Instantiate (Dragonfly, DFPos.position, DFPos.rotation);
+    }
 }
