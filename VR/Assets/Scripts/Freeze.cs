fc4c0fdaf50c5046eb92dc7f59592d6c070ccf25
Mike Dean <msd14@hampshire.edu>
2015-11-01 09:33:44 -0500

    Cleanup

--- VR/Assets/Scripts/Freeze.cs
+++ VR/Assets/Scripts/Freeze.cs
@@ -1,47 +1,47 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class Freeze : MonoBehaviour {
     private Transform character;
-    VariableController variables;
+    private VariableController variables;
     private float speed;
-    AudioSource sound;
-	Transform objTrans;
+    private AudioSource sound;
+    private Transform objTrans;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    private void Start() {
         sound = gameObject.GetComponent<AudioSource>();
         variables = GameObject.Find("Variables").GetComponent<VariableController>();
         speed = variables.projectileSpeed;
         character = Camera.main.transform;
-		objTrans = gameObject.transform;
+        objTrans = gameObject.transform;
+
     }
 
     // Update is called once per frame
-    void Update() {
-		//transform.position = new Vector3(transform.position.x, character.position.y, transform.position.z);
-		transform.position = Vector3.MoveTowards(transform.position, new Vector3(character.position.x * (float)(new System.Random().NextDouble()), character.position.y * (float)(new System.Random().NextDouble()), character.position.z + 1), Time.deltaTime * speed);
+    private void Update() {
+        //transform.position = new Vector3(transform.position.x, character.position.y, transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(character.position.x * (float)(new System.Random().NextDouble()), character.position.y * (float)(new System.Random().NextDouble()), character.position.z + 1), Time.deltaTime * speed);
         if (transform.position.Equals(new Vector3(character.position.x, character.position.y, character.position.z + 1))) {
             Destroy(gameObject);
         }
     }
+
     public void Petrify() {
         if (speed > 1) {
             speed -= speed / 4;
         } else {
             speed = 0;
-			Rigidbody rigid = gameObject.GetComponent<Rigidbody>();
-			//rigid.mass = 1;
-			//rigid.mass *= 1000;
-			rigid.useGravity = true;
-			Invoke ("DestroyObj", 3);
+            Rigidbody rigid = gameObject.GetComponent<Rigidbody>();
+            //rigid.mass = 1;
+            //rigid.mass *= 1000;
+            rigid.useGravity = true;
+            Invoke("DestroyObj", 3);
         }
     }
 
-	public void DestroyObj() {
-		Destroy (gameObject);
-	}
-	
+    public void DestroyObj() {
+        Destroy(gameObject);
+    }
 }
 
 //Pseudo Code
--- VR/Assets/Scripts/Look.cs
+++ VR/Assets/Scripts/Look.cs
@@ -1,26 +1,24 @@
 ﻿using UnityEngine;
-using System.Collections;
+
 //Mike Dean - Script to change color of object when looked at.
 public class Look : MonoBehaviour {
-
-    Camera cam;
-    float t = 0;
-    float duration = 3;
-    VariableController variables;
+    private Camera cam;
+    private float t = 0;
+    private float duration = 3;
+    private VariableController variables;
     public AudioClip[] sounds = new AudioClip[3];
-    Color warning;
+    private Color warning;
 
     // Use this for initialization
-    void Start () {
+    private void Start() {
         //warning = GameObject.Find("Warning").GetComponent<Renderer>().material.color;
         variables = GameObject.Find("Variables").GetComponent<VariableController>();
         cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         DontDestroyOnLoad(gameObject);
-
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    private void Update() {
         MakeRed(new Ray(cam.transform.position, cam.transform.forward));
         //FlashWarning();
         DontDestroyOnLoad(gameObject);
@@ -30,12 +28,9 @@ public class Look : MonoBehaviour {
         }
     }
 
-    public void MakeRed(Ray ray)
-    {
-        
+    public void MakeRed(Ray ray) {
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
-        {
+        if (Physics.Raycast(ray, out hit)) {
             Transform objectHit = hit.transform;
 
             if (objectHit.tag.Equals("Projectile")) {
@@ -48,25 +43,26 @@ public class Look : MonoBehaviour {
 
             Debug.Log(objectHit.name);
             if (objectHit.tag.Equals("Look") || objectHit.tag.Equals("Projectile")) {
-                objectHit.GetComponent<Renderer>().material.color = Color.Lerp(Color.white, Color.red, t);
-                if (t < 1)
-                { // while t below the end limit...
-                  // increment it at the desired rate every update:
+                if (objectHit.name.Substring(0).Equals("S")) {
+                    objectHit.GetComponent<Renderer>().material.color = Color.Lerp(Color.white, Color.green, t);
+                } else {
+                    objectHit.GetComponent<Renderer>().material.color = Color.Lerp(Color.white, Color.red, t);
+                }
+                if (t < 1) { // while t below the end limit...
+                             // increment it at the desired rate every update:
                     t += Time.deltaTime / duration;
                 }
             }
             // Do something with the object that was hit by the raycast.
         }
     }
+
     public void FlashWarning() {
         warning = new Color(warning.r, warning.g, warning.b, 127);
 
         GameObject.Find("Warning").GetComponent<Renderer>().material.color = Color.Lerp(GameObject.Find("Warning").GetComponent<Renderer>().material.color, warning, t);
-        if (t <1) {
+        if (t < 1) {
             t += Time.deltaTime / duration;
         }
     }
-
-
-
 }
